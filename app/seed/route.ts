5643803bcca7a21d787600cb235b53d166cbda5d
Wang, Lei <lei.wang@junlinpro.com>
2025-02-27 18:06:21 +0800

    seed

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,4 +1,6 @@
-import bcrypt from 'bcrypt';
+// import bcrypt from 'bcrypt';
+const bcrypt = require('bcryptjs');
+
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
